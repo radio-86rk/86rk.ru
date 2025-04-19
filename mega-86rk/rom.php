02f2fd7f4c12c36958f999b555d308405ee3b47d
Vital72 <vital72@86rk.ru>
2025-04-19 23:30:26 +0300

    rm mega-86rk dir

--- mega-86rk/rom.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define ('APP_NAME',	'rom');
-	include "../include/app.inc.php";
-?>
--- mega-86rk/sw.js
+++
@@ -1,6 +0,0 @@
-//  Copyright (c) 2022 by August
-//  License: MIT
-//  www:  http://www.86rk.ru/
-//  file: sw.js
-
-
